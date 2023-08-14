8264efdaa8e93ab1b10cd2cd98ba2a897028c813
lbudos17 <leona.budos@gmail.com>
2023-08-14 10:08:41 +0200

    Drugo predavanje

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
 ﻿int broj = 10;
+bool truefalse = true;
+double decimalniBroj = 0.2233;
+float fbroj = 10f;
+
+string ime = "Leona";
+string prezime = "Budoš";
+int age = 24;
+bool gender = true; // žensko true, muško false 
+
+Console.WriteLine("Ime i prezime: {0} {1}", ime, prezime);
+Console.WriteLine("Dob: {0}", age);
+Console.WriteLine("Spol: {0}", gender? "Žensko" : "Muško");
