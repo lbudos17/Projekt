21dd75ccf8488c681f868abbc9d837d092f25c30
lbudos17 <leona.budos@gmail.com>
2023-08-14 16:40:45 +0200

    Zadaća-1

--- Program.cs
+++ Program.cs
@@ -1,13 +1,43 @@
-﻿int broj = 10;
-bool truefalse = true;
-double decimalniBroj = 0.2233;
-float fbroj = 10f;
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-string ime = "Leona";
-string prezime = "Budoš";
-int age = 24;
-bool gender = true; // žensko true, muško false 
+string[] izvor = new string [3] { "biljna hrana", "hrana životinjskog podrijetla", "izlaganje suncu"};
+ 
 
-Console.WriteLine("Ime i prezime: {0} {1}", ime, prezime);
-Console.WriteLine("Dob: {0}", age);
-Console.WriteLine("Spol: {0}", gender? "Žensko" : "Muško");
+Vitamin user = new Vitamin("Vitamin A", "retinol", "vid, imunitet, reproduktivno zdravlje", "noćno sljepilo, sklonost infekcijama", izvor[1], 800, true);
+
+Console.WriteLine("Naziv: {0}", user.ime);
+Console.WriteLine("Drugi naziv: {0}", user.drugo);
+Console.WriteLine("Funkcije: {0}", user.funkcije);
+Console.WriteLine("Nedostaci: {0}", user.nedostatak);
+Console.WriteLine("Izvori: {0}", user.izvor);
+Console.WriteLine("Preporučeni dnevni unos u mcg: {0}", user.unos);
+Console.WriteLine("Vitamin topljiv u: {0}", user.topljivost ? "mastima" : "vodi" );
+
+
+
+
+
+class Vitamin {
+    public string ime;
+    public string drugo; 
+    public string funkcije;
+    public string nedostatak;
+    public string izvor;
+    public int unos;
+    public bool topljivost; //u mastima true, u vodi false
+
+    public Vitamin(string ime, string drugo, string funkcije, string nedostatak, string izvor, int unos, bool topljivost) {
+        this.ime = ime;
+        this.drugo = drugo;
+        this.funkcije = funkcije;
+        this.nedostatak = nedostatak;
+        this.izvor = izvor;
+        this.unos = unos;
+        this.topljivost = topljivost;
+    
+    }
+
+
+
+}
+
